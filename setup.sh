50528d83ff085d0116488654b06ecb2538ed8261
Oliver Kiss <kiss_oliver@phd.ceu.edu>
2020-10-06 14:01:47 +0200

    add setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+pkg install htop
+pkg install python
+pkg install build-essential
+pip install numpy
+export CFLAGS="-Wno-deprecated-declarations -Wno-unreachable-code"
+pip install pandas
+termux-setup-storage
+pkg install git
